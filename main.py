fa92f66cd8c1fa8900445a7bcde149f499f0a56a
frogtwice <frogtwice@proton.me>
2024-07-25 13:11:18 +0200

    app logging

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import os
 
 import eleph
 
+logging.basicConfig(level=logging.INFO)
+
 
 def create_app():
-    logging.basicConfig(level=logging.INFO)
     app = eleph.create_app(
         uri=os.environ.get("URI", "")
     )
-    app.config['DEBUG'] = True
+    app.config['DEBUG'] = os.environ.get("DEBUG") in ("1", "True", "true", "yes", "y")
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
     return app
 
 
