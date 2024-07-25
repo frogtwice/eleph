c9ff87110e98547d856cd2fdc46a42b17da905f9
frogtwice <frogtwice@proton.me>
2024-07-25 13:03:50 +0200

    app debug

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ import eleph
 
 
 def create_app():
-    def run(self, *args, **kwargs):
-        self.logger.info(f"RUNNING WITH {args} {kwargs}")
-        self.run(debug=True)
-
     logging.basicConfig(level=logging.INFO)
     app = eleph.create_app(
         uri=os.environ.get("URI", "")
     )
-    app.run = run
+    app.config['DEBUG'] = True
     return app
 
 
